b8ac06a0f910b0571bd0a17c95f53acbecaf21e4
Maxsuel Aparecido Lima Santos <72948249+Maxsuel-Santos@users.noreply.github.com>
2023-03-20 20:39:58 -0300

    Update aula06.py

---
+++ POO/aula06.py
@@ -0,0 +1,36 @@
+class Camera:
+    def __init__(self, nome, filmando = False):
+        self.nome = nome
+        self.filmando = filmando
+        
+    def filmar(self):
+        if self.filmando:
+            print(f'{self.nome} JÁ está filmando')
+            return
+        
+        print(f'{self.nome} está filmando.')
+        self.filmando = True
+        
+    def fotografar(self):
+        if self.filmando:
+            print(f'{self.nome} NÃO pode fotografar filmando.')
+            return
+        
+        print(f'{self.nome} está fotografando.')
+        
+    def paraDeFilmar(self):
+        if not self.filmando:
+            print(f'{self.nome} NÃO está filmando')
+            return
+        
+        print(f'{self.nome} está parando de filmar.')
+        self.filmando = False
+        
+c1 = Camera('Canon')
+c1.filmar()
+c1.fotografar()
+c1.paraDeFilmar()
+c1.fotografar()
+
+c2 = Camera('Sony')
+c2.filmar()
