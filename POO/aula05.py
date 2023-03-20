015bcba266952d87e1d3bfd5fda5da47321717c2
Maxsuel Aparecido Lima Santos <72948249+Maxsuel-Santos@users.noreply.github.com>
2023-03-20 15:57:56 -0300

    Update aula05.py

--- POO/aula05.py
+++ POO/aula05.py
@@ -1,1 +1,10 @@
 # Escopo da classe e de métodos da classe
+class Animal:
+    def __init__(self, nome):
+        self.nome = nome
+        
+    def alimentar(self, alimento):
+        return f'O {self.nome} está comendo {alimento}.'
+    
+leao = Animal(nome = 'Leão')
+print(leao.alimentar('Beterraba'))
