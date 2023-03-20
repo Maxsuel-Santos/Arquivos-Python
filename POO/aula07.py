d9a7cc636a2895a6520b41dc0af7b71318787a36
Maxsuel Aparecido Lima Santos <72948249+Maxsuel-Santos@users.noreply.github.com>
2023-03-20 20:56:24 -0300

    Update aula07.py

--- POO/aula07.py
+++ POO/aula07.py
@@ -1,1 +1,17 @@
 # Atributo de classe
+class Pessoa:
+    anoAtual = 2022
+    
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+        
+    # Função apenas demonstratica, essa não é a melhor forma de calcular idade
+    def get_anoNascimento(self):
+        return Pessoa.anoAtual - self.idade
+    
+p1 = Pessoa('João', 35)
+print(p1.get_anoNascimento())
+
+p2 = Pessoa('Helena', 12)
+print(p2.get_anoNascimento())
